ca9031d2614006f5c2e8d061e095523207089cb2
Tarmim Hoque Shuvo <tarmimhoqueshuvo@Tarmims-MacBook-Pro.local>
2020-06-28 11:33:53 +0600

    add listItem component

---
+++ src/components/LisItem.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <p
+        id= "p1"
+        class="msgTxt"
+        @mouseover="dim"
+        >
+        <!--         :class="{msgTxt:true}"  you have to bind class if there is a condition -->
+            {{title}}
+        </p>
+    </div>
+</template>
+<script>
+export default {
+    name: "ListItem",
+    props: ["naame"],
+    computed:{
+        title(){
+            return 'we'+ ' '+"like "+this.naame.title
+            // That;s how you use props
+            }
+    },
+    methods: {
+        dim(){
+            console.log("props.naame",this.naame)
+            // this.naame.settitle("hudamisa")
+        }
+    },
+    
+}
+</script>
+<style scoped>
+#p1{
+    font-size: 40;
+}
+.msgTxt {
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  color: azure;
+  font-weight: bolder;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld :lists="lists" msg="Welcome"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  data(){
+    return{
+      lists: [
+        {
+          id:1,
+          title: "one"
+        },
+        {
+          id:2,
+          title: 'two'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.msgTxt {
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  color: brown;
+  font-weight: bolder;
+}
+
+</style>
